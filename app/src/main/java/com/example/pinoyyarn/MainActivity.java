9a64ae645b7d44435f445f6a06452778193dd052
Sean Aldridge Francisco <seanaldridgemfrancisco@iskolarngbayan.pup.edu.ph>
2021-12-12 16:44:18 +0800

    Added confirm exit function

--- app/src/main/java/com/example/pinoyyarn/MainActivity.java
+++ app/src/main/java/com/example/pinoyyarn/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.pinoyyarn;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -24,5 +26,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public void onBackPressed(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Are yoy sure you want to exit?");
+        builder.setCancelable(true);
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface2, int which) {
+                finish();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
