b0e69551731e7c2f5366f520144297e0bd44ee8f
12345 <aidyabes13@gmail.com>
2021-12-14 22:04:23 +0800

    One

--- app/src/main/java/com/example/pinoyyarn/MainActivity.java
+++ app/src/main/java/com/example/pinoyyarn/MainActivity.java
@@ -3,17 +3,26 @@ package com.example.pinoyyarn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton start_sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start_sound = (ImageButton)findViewById(R.id.btn_start);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.clicksound);
+        start_sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
 
         ImageButton btn_ToMenuScreen = (ImageButton) findViewById(R.id.btn_start);
         btn_ToMenuScreen.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent;
                 intent = new Intent(MainActivity.this,MenuActivity.class);
                 startActivity(intent);
+            }
+        });
             }
         });
     }
--- app/src/main/java/com/example/pinoyyarn/MenuActivity.java
+++ app/src/main/java/com/example/pinoyyarn/MenuActivity.java
@@ -2,7 +2,10 @@ package com.example.pinoyyarn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        ImageButton btn_ToPlayScreen = (ImageButton) findViewById(R.id.btn_play);
+        btn_ToPlayScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(MenuActivity.this,CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
