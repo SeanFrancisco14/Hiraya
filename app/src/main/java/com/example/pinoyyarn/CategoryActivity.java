76ab5db55997a3aee1c5477c1252e33585d2de8f
12345 <aidyabes13@gmail.com>
2021-12-15 17:59:50 +0800

    History and Language STAGES + Achievements and Setting pages

--- app/src/main/java/com/example/pinoyyarn/CategoryActivity.java
+++ app/src/main/java/com/example/pinoyyarn/CategoryActivity.java
@@ -5,22 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class CategoryActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
-        findViewById(R.id.btn_culture).setOnClickListener(this);
+        ImageButton btn_culture = (ImageButton) findViewById(R.id.btn_culture);
+        ImageButton btn_history = (ImageButton) findViewById(R.id.btn_history);
+        ImageButton btn_language = (ImageButton) findViewById(R.id.btn_language);
+
+        btn_culture.setOnClickListener(this);
+        btn_history.setOnClickListener(this);
+        btn_language.setOnClickListener(this);
     }
 
-    public void onClick(View view) {
-        startActivity(new Intent(this, CultureStages.class));
-
-        if (view.getId() == R.id.btn_culture) {
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_culture:
+                startActivity(new Intent(this, CultureStages.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                break;
+        }
+        switch(v.getId()) {
+            case R.id.btn_history:
+                startActivity(new Intent(this, HistoryStages.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);;
+                break;
+        }
+        switch(v.getId()) {
+            case R.id.btn_language:
+                startActivity(new Intent(this, LanguageStages.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                break;
         }
     }
 }
---
+++ app/src/main/java/com/example/pinoyyarn/LanguageStages.java
@@ -0,0 +1,14 @@
+package com.example.pinoyyarn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LanguageStages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language_stages);
+    }
+}
