256f5af18ea055dcda53ee1c4bbb03e11ee037bb
12345 <aidyabes13@gmail.com>
2021-12-15 14:11:46 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/pinoyyarn/MenuActivity.java
+++ app/src/main/java/com/example/pinoyyarn/MenuActivity.java
@@ -7,21 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        ImageButton btn_ToPlayScreen = (ImageButton) findViewById(R.id.btn_play);
-        btn_ToPlayScreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent;
-                intent = new Intent(MenuActivity.this,CategoryActivity.class);
-                startActivity(intent);
-            }
-        });
+        findViewById(R.id.btn_play).setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, CategoryActivity.class));
+
+        switch (view.getId()) {
+            case R.id.btn_play:
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
     }
 }
