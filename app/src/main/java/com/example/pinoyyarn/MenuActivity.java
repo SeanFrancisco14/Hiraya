bcf0c6816885e1a6289535a27ca5615455d62b30
Sean Aldridge Francisco <seanaldridgemfrancisco@iskolarngbayan.pup.edu.ph>
2021-12-23 12:46:55 +0800

    Optimized code for main activity and menu file. Resized activity_category.xml image button so that they are the same size when running

--- app/src/main/java/com/example/pinoyyarn/MenuActivity.java
+++ app/src/main/java/com/example/pinoyyarn/MenuActivity.java
@@ -17,10 +17,10 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        ImageButton btn_play = (ImageButton) findViewById(R.id.btn_play);
-        ImageButton btn_achievements = (ImageButton) findViewById(R.id.btn_achievements);
-        ImageButton btn_settings = (ImageButton) findViewById(R.id.btn_settings);
-        ImageButton btn_exit = (ImageButton) findViewById(R.id.btn_exit);
+        ImageButton btn_play = findViewById(R.id.btn_play);
+        ImageButton btn_achievements = findViewById(R.id.btn_achievements);
+        ImageButton btn_settings = findViewById(R.id.btn_settings);
+        ImageButton btn_exit = findViewById(R.id.btn_exit);
 
         btn_play.setOnClickListener(this);
         btn_achievements.setOnClickListener(this);
@@ -30,43 +30,26 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.btn_play:
-                startActivity(new Intent(this, CategoryActivity.class));
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                break;
+        if (v.getId() == R.id.btn_play) {
+            startActivity(new Intent(this, CategoryActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         }
-        switch(v.getId()) {
-            case R.id.btn_achievements:
-                startActivity(new Intent(this, AchievementsActivity.class));
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);;
-                break;
+        if (v.getId() == R.id.btn_achievements) {
+            startActivity(new Intent(this, AchievementsActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         }
-        switch(v.getId()) {
-            case R.id.btn_settings:
-                startActivity(new Intent(this, SettingsActivity.class));
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                break;
+        if (v.getId() == R.id.btn_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         }
-        switch(v.getId()) {
-            case R.id.btn_exit:
-                final AlertDialog.Builder builder = new AlertDialog.Builder(MenuActivity.this);
-                builder.setMessage(Html.fromHtml("<font color='#000000'>Are you sure you want to exit?</font>"));
-                builder.setCancelable(true);
-                builder.setPositiveButton(Html.fromHtml("<font color='#FFD700'>Yes</font>"), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                });
-                builder.setNegativeButton(Html.fromHtml("<font color='#FFD700'>No</font>"), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int i) {
-                        dialog.cancel();
-                    }
-                });
-                AlertDialog alertDialog = builder.create();
-                alertDialog.show();
+        if (v.getId() == R.id.btn_exit) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(MenuActivity.this);
+            builder.setMessage(Html.fromHtml("<font color='#000000'>Are you sure you want to exit?</font>"));
+            builder.setCancelable(true);
+            builder.setPositiveButton(Html.fromHtml("<font color='#FFD700'>Yes</font>"), (dialog, which) -> finish());
+            builder.setNegativeButton(Html.fromHtml("<font color='#FFD700'>No</font>"), (dialog, i) -> dialog.cancel());
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
         }
     }
 }
