25eb908f9904ab984cbbd63053955ca3db3737f6
Sean Aldridge Francisco <seanaldridgemfrancisco@iskolarngbayan.pup.edu.ph>
2022-02-06 13:02:25 +0800

    added working incremental volume control, background music next.

--- app/src/main/java/com/example/pinoyyarn/SettingsActivity.java
+++ app/src/main/java/com/example/pinoyyarn/SettingsActivity.java
@@ -1,57 +1,62 @@
 package com.example.pinoyyarn;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener{
 
     private AudioManager audioManager;
-    private Button btnCredits, btnOn, btnOff;
+    private Button btnCredits, volumeControlUp, volumeControlDown;
+    private SeekBar seekBar;
 
-
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        volumeControlUp = findViewById(R.id.volumeControlUp);
+        volumeControlDown = findViewById(R.id.volumeControlDown);
         btnCredits= findViewById(R.id.btnCredits);
-        btnOn = findViewById(R.id.btnSoundOn);
-        btnOff = findViewById(R.id.btnSoundOff);
+        seekBar = findViewById(R.id.seekBarVol);
 
-
-        btnOn.setOnClickListener(this);
-        btnOff.setOnClickListener(this);
+        volumeControlUp.setOnClickListener(this);
+        volumeControlDown.setOnClickListener(this);
         btnCredits.setOnClickListener(this);
+
+        seekBar.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        seekBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     public void onClick(View v) {
-        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
-        if (v.getId() == R.id.btnSoundOn) {
-            audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-            audioManager.setStreamVolume(AudioManager.STREAM_RING, maxVolume, AudioManager.FLAG_SHOW_UI + AudioManager.FLAG_PLAY_SOUND);
+        if (v.getId() ==  R.id.volumeControlUp) {
+           audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
+           seekBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+            Toast.makeText(SettingsActivity.this, "Volume up", Toast.LENGTH_SHORT).show();
         }
-        if (v.getId() == R.id.btnSoundOff) {
-            audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
-        }
-
-        int mod = audioManager.getRingerMode();
-        if (mod == audioManager.RINGER_MODE_VIBRATE){
-            Toast.makeText(SettingsActivity.this, "sounds are off", Toast.LENGTH_SHORT).show();
-        }
-        else if (mod ==audioManager.RINGER_MODE_NORMAL){
-            Toast.makeText(SettingsActivity.this, "sounds are on", Toast.LENGTH_SHORT).show();
+        if (v.getId() == R.id.volumeControlDown) {
+            audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND);
+            seekBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+            Toast.makeText(SettingsActivity.this, "Volume down", Toast.LENGTH_SHORT).show();
         }
     }
 }
