fd7f476104ca8756dad75ae8f2eb0df8cb9aa8f0
Danica Pamoleras <danicagpamoleras@iskolarngbayan.pup.edu.ph>
2022-02-16 03:20:53 +0800

    CULTURE

--- app/src/main/java/com/example/pinoyyarn/LEasyQ2.java
+++ app/src/main/java/com/example/pinoyyarn/LEasyQ2.java
@@ -53,16 +53,16 @@ public class LEasyQ2 extends AppCompatActivity implements View.OnClickListener {
     private void getLanEasyQuestions() {
         questionList = new ArrayList<>();
 
-        questionList.add(new LEasyQ1_List("tae ka!", "A. Apolinario Mabini", "B. Emilio Jacinto", "C. Andres Bonifacio", "D. Jose Rizal", 3));
-        questionList.add(new LEasyQ1_List("Who is the national hero of the Philippines?", "A. Andres Bonifacio", "B. Jose Rizal", "C. Emilio Aguinaldo", "D. Gregorio Del Pilar", 2));
-        questionList.add(new LEasyQ1_List("Who is generally acknowledged as the first President of the Philippines?", "A. Emilio Aguinaldo", "B. Manuel L. Quezon", "C. Andres Bonifacio", "D. Manuel Roxas", 1));
-        questionList.add(new LEasyQ1_List("She is a Filipino heroine. After her husband died, she continued the war against Spain, was caught and hanged.", "A. Teodora Alonso", "B. Gregoria de Jesus", "C. Gabriela Silang", "D. Leonor Rivera", 3));
-        questionList.add(new LEasyQ1_List("She was the first woman member of the Katipunan (July 1893).", "A. Gregoria de Jesus", "B. Segunda Katikbak", "C. Gabriela Silang", "D. Marina Dizon", 1));
-        questionList.add(new LEasyQ1_List("He is known as the founder and 'Father of Katipunan', 'Supremo', or the 'Great Plebeian'.", "A. Gregorio del Pilar", "B. Andres Bonifacio", "C. Antonio Luna", "D. Emilio Jacinto", 2));
-        questionList.add(new LEasyQ1_List("It is the town is Zamboanga del Norte where Dr. Jose Rizal was exiled for four years before he was executed.", "A. Dipolog", "B. Ipil", "C. Dagonoy", "D. Dapitan", 4));
-        questionList.add(new LEasyQ1_List("Dr. Jose Rizal wrote this poem before he was executed.", "A. Mi Ultimo Adios", "B. La Solidaridad", "C. Noli Me Tangere", "D. Ibong Adarna", 1));
-        questionList.add(new LEasyQ1_List("Who was the chief advisor of Gen. Emilio Aguinaldo?", "A. Felipe Calderon", "B. Apolinario Mabini", "C. Pedro Paterno", "D. Jose Rizal", 2));
-        questionList.add(new LEasyQ1_List("We celebrate the Araw ng Kagitingan every_______________.", "A. February 27", "B. April 9", "C. June 24", "D. December 30", 2));
+        questionList.add(new LEasyQ1_List("Pick the right correct spelling for ‘electricity or spark’. ", "Tagitab", "Dagitab", "Dagatab", "Dugitab", 2));
+        questionList.add(new LEasyQ1_List("Pick the right correct spelling for ‘last farewell’", "Pakimas", "Pahikimas", "Pahikamas", "Pahukamis", 3));
+        questionList.add(new LEasyQ1_List("Pick the right correct spelling for ‘Calendar’", "Talarawan", "Talaaraw-araw", "Talaaraw", "Talaarawan", 4));
+        questionList.add(new LEasyQ1_List("Pick the right correct spelling for ‘Agriculture’.", "Dalsakahan", "Dalubsakahan", "Dalubdubsaka", "Dalubsakasaka", 2));
+        questionList.add(new LEasyQ1_List("Pick the right correct spelling for ‘Charger’. ", "Pantablay", "Pantabla", "Panablay", "Pantabay", 1));
+        questionList.add(new LEasyQ1_List("Pick the right correct spelling for ‘Morning’. ", "Liwayway", "Bukhang Liwayway", "Bukang Liwayway", "Bukay Liwliway", 3));
+        questionList.add(new LEasyQ1_List("Pick the right correct spelling for ‘Faucet’", "Batalaan", "Bataan", "Baatan", "Batalan", 4));
+        questionList.add(new LEasyQ1_List("Pick the right correct spelling for ‘Someone you’re talking too”.", "Kalipun", "Kalipon", "Kulipon", "Kulipun", 2));
+        questionList.add(new LEasyQ1_List("Pick the right correct spelling for ‘Integer’", "Buongbilang", "Bumbilang", "Buumbilang", "Buobilang", 3));
+        questionList.add(new LEasyQ1_List("Pick the right correct spelling for ‘to continue’.", "Padayon", "Padahyun", "Padayun", "Padayeon", 1));
 
         setQuestion();
     }
