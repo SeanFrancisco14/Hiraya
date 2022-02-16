39d7408e520237e8d2f12cab8a8455f0109549fc
Danica Pamoleras <danicagpamoleras@iskolarngbayan.pup.edu.ph>
2022-02-16 17:44:48 +0800

    CULTURE

--- app/src/main/java/com/example/pinoyyarn/cHardQ2.java
+++ app/src/main/java/com/example/pinoyyarn/cHardQ2.java
@@ -53,12 +53,31 @@ public class cHardQ2 extends AppCompatActivity implements View.OnClickListener {
     private void getCultEasyQuestions() {
         questionList = new ArrayList<>();
 
-        questionList.add(new cEasyQ1_List("culture hard 2", "A", "B", "C", "D", 1));
-        questionList.add(new cEasyQ1_List("Question 2", "B", "B", "D", "C", 1));
-        questionList.add(new cEasyQ1_List("Question 3", "C", "D", "A", "A", 1));
-        questionList.add(new cEasyQ1_List("Question 4", "C", "D", "A", "B", 2));
-        questionList.add(new cEasyQ1_List("Question 5", "A", "D", "B", "C", 4));
-
+        questionList.add(new cEasyQ1_List("Who is the highest American military official that was killed in action during the Filipino-American war?", "Col. Grayson", "Gen. Pershing", "Gen. Lawton", "Gen. Merrit", 3));
+        questionList.add(new cEasyQ1_List("What is the name of Filipino armed contingent that helped the Americans capture Aguinaldo?", "Fire Brigade", " Balangiga Rangers", "Macabebe Scouts", "Abu Sayyaf", 3));
+        questionList.add(new cEasyQ1_List("One of the last Filipino generals who fought the Americans and established the so-called 'Tagalog Republic’.", "Macario Sakay", "Manuel Tinio", "Miguel Malvar", "Gregorio del Pilar", 1));
+        questionList.add(new cEasyQ1_List("Who was the last general of the Filipino-American revolution to surrender to the Americans?", "Miguel Malvar", "Cornelio Felizardo", "Artemio Ricarte", "Simeon Ola", 4));
+        questionList.add(new cEasyQ1_List("It is the law which allowed the establishment of American Civil Government in the Philippines.", "Tydings-McDuffie Law", "Jone’s Law of 1916", "Taft’s Law", "Warren Law", 2));
+        questionList.add(new cEasyQ1_List("He was a former President of the USA who appointed General MacArthur to rule and govern the Philippines.", "Dwight D. Eisenhower", "Franklin D. Roosevelt", "George Washington", "William Howard Taft", 4));
+        questionList.add(new cEasyQ1_List("Who was the American President who declared the Benevolent Assimilation?", "William McKinley", "Theodore Roosevelt", "Franklin Roosevelt", "Warren Harding", 1));
+        questionList.add(new cEasyQ1_List("Which recommended that US should remain in the Philippines since our country was not yet ready for independence?", "Jones Law", "Tydings-McDuffie Law", "Schurman Commission", "Taft Commission", 2));
+        questionList.add(new cEasyQ1_List("Which provided for a 10-year transition period under Commonwealth?", "Tydings-McDuffie Law", "Jones Law", "Schurman Commission", "Taft Commission", 1));
+        questionList.add(new cEasyQ1_List("Which of the following laws of the Americans defined the structure, the power and duties of the three branches of government in the Philippines?", "Jones Law", "Hare-Hawes Cutting Act", "Tydings-McDuffie Law", "Cooper Act", 1));
+        questionList.add(new cEasyQ1_List("__________________ provided the US bill of rights for the Filipino people", "Cooper Act", "Taft Commission", "Hare-Hawes Cutting Act", "Tydings-McDuffie", 2));
+        questionList.add(new cEasyQ1_List("_________________ established a civil government and train Filipinos for self-government.", "Cooper Act", "Jones Law", "Taft Commission", "Wood-Forbes Mission", 1));
+        questionList.add(new cEasyQ1_List("What was the evidence that the Americans dominate the provincial government?", "The Filipinos are not allowed to vote for the provincial government", "The officers for the provincial government were appointed by the Central Government", "The influential positions were under the Americans", "The American Commissioner supervise the provincial government", 3));
+        questionList.add(new cEasyQ1_List("Why did the elite dominate the local government?", "Because they had the money to buy votes", "Because they are the well-educated", "Because the masses had no interest", "Because of the qualifications for suffrage", 4));
+        questionList.add(new cEasyQ1_List("When was the civil government in the Philippines officially inaugurated?", "August 9, 1991", "May 16, 1901", "July 4, 1901", "July 4, 1908", 3));
+        questionList.add(new cEasyQ1_List("This amendment called for the establishment of a civil government in the Philippines. ", "Taft Amendment", "Spooner Amendment", "Mckinley Amendment", "Jones Amendment", 2));
+        questionList.add(new cEasyQ1_List("He was the US President who ordered the colonization of the Philippines.", "Henry Cooper", "William Mckinley", "Theodore Roosevelt", "Jacob Schurman", 2));
+        questionList.add(new cEasyQ1_List("What was the name of the United States Navy ship that sank in Havana Harbor in February 1898, contributing to the outbreak of the Spanish–American War?", "USS Dolphin", "USS Maine", " U.S.S. Brooklyn", "U.S.S. Marblehead", 2));
+        questionList.add(new cEasyQ1_List("Where was the USS Maine anchored?", "Trinidad, Cuba", "Los Angeles", "Corregidor Island", "Havana, Cuba", 4));
+        questionList.add(new cEasyQ1_List("Who led the American fleet who arrived at the Manila bay on May 1, 1898?", "General Fermin Faudenes", "Major General Wesley Merritt", "Commodore George Dewey", "General Francis Green", 3));
+        questionList.add(new cEasyQ1_List("When did the Mock battle took place?", "August 13,", "August 13,1898", "August 14,1898", "August 13,1897", 2));
+        questionList.add(new cEasyQ1_List("Who is the Spanish fleet commander who fought against the American fleet led by Commodore George Dewey?", "General Francis Green", "Emilio Aguinaldo", "Governor General Fermin Jaudenes", "Admiral Patricio Montojo", 4));
+        questionList.add(new cEasyQ1_List("He is a Spanish commander and also one of the people who knew about the Mock battle in Manila.", "General Francis Green", "General Wesley Merritt", "Governor General Fermin Jaudenes", "Admiral Patricio Montojo", 3));
+        questionList.add(new cEasyQ1_List("Theodore Roosevelt and his 'Rough Riders' fought at which battle?", "The Battle of Sand Wand Hill", "The Battle of Manila Bay", "The Battle of Madrid", "The Battle of San Juan Hill", 4));
+        questionList.add(new cEasyQ1_List("_____________ was elected as the president of the Constitutional Convention in 1934.", "Manuel Quezon", "Emilio Aguinaldo", "Claro Recto", "Cayetano Arellano", 3));
 
         setQuestion();
     }
