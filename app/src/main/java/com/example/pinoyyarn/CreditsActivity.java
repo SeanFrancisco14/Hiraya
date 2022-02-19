6a01c49fa4334c25ac9edc641a4fe17e4bc473d2
Sean Aldridge Francisco <seanaldridgemfrancisco@iskolarngbayan.pup.edu.ph>
2022-02-19 17:24:03 +0800

    audioClass

---
+++ app/src/main/java/com/example/pinoyyarn/CreditsActivity.java
@@ -0,0 +1,19 @@
+package com.example.pinoyyarn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreditsActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
