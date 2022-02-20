4841794c31d52a3ea33ea27282b70aaad10915e8
Adrian Yabes <82687799+adrianyabes@users.noreply.github.com>
2022-02-20 13:45:56 +0800

    aesthetics and score activity button changes

--- app/src/main/java/com/example/pinoyyarn/cScoreActivity.java
+++ app/src/main/java/com/example/pinoyyarn/cScoreActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class cScoreActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView score;
-    private Button confirm;
+    private ImageButton confirm;
+    private ImageButton menuback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +21,24 @@ public class cScoreActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_cscore);
 
         score = findViewById(R.id.textView_cScore);
-        confirm = findViewById(R.id.cScoreButton);
+        confirm = findViewById(R.id.chooseanothercat);
+        menuback = findViewById(R.id.backtomenu);
 
         String score_str = getIntent().getStringExtra("SCORE");
         score.setText(score_str);
 
         confirm.setOnClickListener(this);
+        menuback.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.cScoreButton) {
+        if (v.getId() == R.id.chooseanothercat) {
             startActivity(new Intent(this, CategoryActivity.class));
+            cScoreActivity.this.finish();
+        }
+        if (v.getId() == R.id.backtomenu) {
+            startActivity(new Intent(this, MenuActivity.class));
             cScoreActivity.this.finish();
         }
     }
--- app/src/main/java/com/example/pinoyyarn/hScoreActivity.java
+++ app/src/main/java/com/example/pinoyyarn/hScoreActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class hScoreActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView score;
-    private Button confirm;
+    private ImageButton backtocat;
+    private ImageButton backtomenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +20,26 @@ public class hScoreActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_hscore);
 
         score = findViewById(R.id.textView_hScore);
-        confirm = findViewById(R.id.hScoreButton);
+        backtocat = findViewById(R.id.backtocat);
+        backtomenu = findViewById(R.id.backtomenu);
 
         String score_str = getIntent().getStringExtra("SCORE");
         score.setText(score_str);
 
-        confirm.setOnClickListener(this);
+        backtocat.setOnClickListener(this);
+        backtomenu.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.hScoreButton) {
+        if (v.getId() == R.id.backtocat) {
             startActivity(new Intent(this, CategoryActivity.class));
             hScoreActivity.this.finish();
          }
+
+        if (v.getId() == R.id.backtomenu) {
+            startActivity(new Intent(this, MenuActivity.class));
+            hScoreActivity.this.finish();
+        }
     }
 }
