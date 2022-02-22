0b177e8231786d691f1640c446838febfc79d0d4
Danica Pamoleras <danicagpamoleras@iskolarngbayan.pup.edu.ph>
2022-02-22 16:50:14 +0800

    CULTURE

--- app/src/main/java/com/example/pinoyyarn/cMedQ1.java
+++ app/src/main/java/com/example/pinoyyarn/cMedQ1.java
@@ -53,21 +53,21 @@ public class cMedQ1 extends AppCompatActivity implements View.OnClickListener {
     private void getCultEasyQuestions() {
         questionList = new ArrayList<>();
 
-        questionList.add(new cEasyQ1_List("The dance's place of origin is at Lingayen, Pangasinan and dancers are using sticks to perform.", "Tinikling", "Sua-ku-sua", "Pangalay", "Biniskalan", 4));
-        questionList.add(new cEasyQ1_List("Philippine Folk Dance mirrors the _____ of the Filipinos.", "Beliefs", "Value and moral", "Culture and Tradition", "Knowledge and wisdom", 3));
-        questionList.add(new cEasyQ1_List("It is performed mainly during weddings and other festive events.", "Binislakan", "Pangalay", "Tinikling", "Bahay-Kubo", 2));
-        questionList.add(new cEasyQ1_List("One of the most famous dances in the Philippines that uses bamboo poles.", "Tinikling", "Itik-Itik", "Maglalatik", "Pantomina", 1));
-        questionList.add(new cEasyQ1_List("A folk dance in Luzon that is a mock dance over a fight over coconut meat, a highly prized food.", "Maglalatik", "Itik-itik", "Pantomina", "Binasuan", 1));
-        questionList.add(new cEasyQ1_List("This dance is performed on the top of a narrow chair that originated in Pangasinan.", "Itik-itik", "Pantomina", "Sayaw sa Bangko", "Binasuan", 3));
-        questionList.add(new cEasyQ1_List("A folk dance from Pangasinan that uses drinking glasses that the dancers balances in their head and hand as they move.", "Pantomina", "Itik-itik", "Pantomina", "Binasuan", 4));
-        questionList.add(new cEasyQ1_List("Is a cultural dance in the Philippines influenced by Muslims that is originated by the Maranao tribe in Mindanao.", "Kappa Malong-malong", "Binasuan", "Pantomina", "Itik-itik", 1));
-        questionList.add(new cEasyQ1_List("This is typically performed by fishermen to celebrate a bountiful catch.", "Itik-itik", "Pantomina", "Pandanggo Oasiwas", "Kuratsa", 3));
-        questionList.add(new cEasyQ1_List("It is another courtship dance in the Philippines that is said to mimic the movement of doves in courtship.", "Pantomina", "Pandaggo sa Ilaw", "Kuratsa", "Itik-Itik", 1));
-        questionList.add(new cEasyQ1_List("Where does Kuratsa originated from?", "Bicol Region", "Pangasinan", "Samar Island", "Batanes", 3));
-        questionList.add(new cEasyQ1_List("Known for her abstract ballets to music Rhapsody in Blue, Rhapsody on a Theme of Paganini, Serenade for Strings, Cappriccio Espagnol.", "Rosalia Merino Santos", "Julie Borromeo", "Leonor Orosa Goquingco", "Remedios de Oteyza", 4));
-        questionList.add(new cEasyQ1_List("Which of these is not a step of Polkabal?", "uksong Uwak", "Corriba", "Paseo", "Ensayo", 2));
-        questionList.add(new cEasyQ1_List("It is a comic dance from Ilocos sur that depicts the movement of a person bitten by ants.", "Kinoton", "Habanera", "Kuratsa", "Itik-itik", 1));
-        questionList.add(new cEasyQ1_List("It is a mix of Cuban, Spanish and African rhythms and dance moves.", "Kinoton", "Kuratsa", "Habanera", "Itik-itik", 3));
+        questionList.add(new cEasyQ1_List("A series of recordings made on music played on the spot and has to be performed once.", "Primal Music", "Gong Music", "Indigenous Music", "Rondalla Music", 1));
+        questionList.add(new cEasyQ1_List("Racked gong chime instrument played in the southern islands of the Philippines.", "Gangsa", "Kulintang", "Rondalla", "Tinikling", 2));
+        questionList.add(new cEasyQ1_List("Rooted in the Mexican-Spanish tradition and based on the rhythmic patterns of the habanera.", "Gangsa", "Harana", "Kundiman", "Rondalla", 2));
+        questionList.add(new cEasyQ1_List("He is known for the witty song ‘Butse Kik’.", "Willie Revillame", "Rey Valera", "Yoyoy Villame", "Ely Buendia", 3));
+        questionList.add(new cEasyQ1_List("His songs are ‘Maging Sino Ka Man’, ‘Pangako Sa’yo’, ‘Kahit Wala Ka Na’ and many more.", "Willie Revillame", "Rey Valera", "Yoyoy Villame", "Ely Buendia", 2));
+        questionList.add(new cEasyQ1_List("He has 39 Albums and won the Awit Award for the ‘Best Male Performer’ 12 times.", "Ely Buendia", "Gary Valenciano", "Ryan Cayabyab", "Ogie Alcasid", 2));
+        questionList.add(new cEasyQ1_List("What is the term that gave rise to songs using colloquial language?", "Manila Sound", "OPM", "Rap", "Pinoy Rock", 1));
+        questionList.add(new cEasyQ1_List("He led the first Philippine performance of Bach's Christmas Oratorio.", "Rey Valera", "Antonio Molina", "Jose Maceda", "Gary Valenciano", 2));
+        questionList.add(new cEasyQ1_List("It is a two-stringed boat-shaped lute.", "Kudyapi", "Rondalla", "Kudlung", "Saludoy", 1));
+        questionList.add(new cEasyQ1_List("She was known for being instrumental to the development of Philippine music and culture.", "Lucresia Roces Kasipag", "Lucresia Roses Kasipag", "Lucresia Roces Kasilag", "Lucresia Roses Kasilag", 3));
+        questionList.add(new cEasyQ1_List("The Golden Era of ballad in the Philippines is during ____.", "1980s", "1990s", "1970s", "1960s", 1));
+        questionList.add(new cEasyQ1_List("These are instruments that are plucked.", "Idiophones", "Chordophones", "Aerophones", "Membranophones", 3));
+        questionList.add(new cEasyQ1_List("She was hailed as 'The Queen of Filipino Jazz' and as 'The Queen of Bodabil'.", "Ciara Sotto", "Lucresia Roces Kasilag", "Katy dela Cruz", "Ayen Laurel", 3));
+        questionList.add(new cEasyQ1_List("He was named as the National Artist of the Philippines for Music in 2018.", "Ely Buendia", "Rico Blanco", "Jay-R Siaboc", "Rey Cayabyab", 4));
+        questionList.add(new cEasyQ1_List("Who is the first Filipino singer to do a local version of an English rock song?", "Ariel Rivera", "Rico Blanco", "Rico Pun", "Bobby Gonzales", 4));
 
         setQuestion();
     }
